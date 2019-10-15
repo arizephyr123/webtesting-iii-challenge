5fcaad984e1590936c5e6ccfa79864372f8a3726
Arielle Hollingsworth <arizephyr123@gmail.com>
2019-10-15 13:45:32 -0700

    error preventing tests to run

--- src/dashboard/Dashboard.spec.js
+++ src/dashboard/Dashboard.spec.js
@@ -1,1 +1,14 @@
 // Test away
+
+import React from 'react';
+import { render } from '@testing-library/react';
+
+import Dashboard from './Dashboard';
+
+
+describe('tests for Dashboard Component', () => {
+    test('matches snapshot', ()=>{
+        const tree = render(<Dashboard/>)
+        expect((<Dashboard/>).baseElement).toMatchSnapshot()
+    })
+})
